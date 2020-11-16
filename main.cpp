ecf6983ea038570067eef1b3c7164eae95ea3479
Nik <nikibar4@gmail.com>
2020-11-16 20:06:00 +0200

    Pointers and C-style strings

--- main.cpp
+++ main.cpp
@@ -17,16 +17,8 @@ int* find(int *first, int *last, int value)
  
 int main()
 {
-    int arr[]{ 2, 5, 4, 10, 8, 20, 16, 40 };
- 
-    // Search for the first element with value 20.
-    int *found{ find(std::begin(arr), std::end(arr), 20) };
- 
-    // If an element with value 20 was found, print it.
-    if (found != std::end(arr))
-    {
-        std::cout << *found << '\n';
-    }
+    char c{ 'Q' };
+    std::cout << &c;
  
     return 0;
 }
